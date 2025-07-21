a99c41b3f9e0ed4e3885fdf76954cc2ffeeadd81
Evgeniy Zherzdev <zherzdev.en@sibvaleo.com>
2025-07-21 14:14:39 +0300

    Soldved

--- main.go
+++ main.go
@@ -1,4 +1,31 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(makeFancyString("a"))
+}
+
+func makeFancyString(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	var result []byte
+	count := 1
+	result = append(result, s[0]) // обязательно добавляем первый символ
+
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			count++
+		} else {
+			count = 1
+		}
+
+		if count < 3 {
+			result = append(result, s[i])
+		}
+	}
+
+	return string(result)
 }
